d0212e4fb23147a3d8bdb20c262cc41767e6e0aa
lucgus11 <lucas.gustin@indse.be>
2026-03-23 09:53:18 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,18 @@
+// next.config.js
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  buildExcludes: [/middleware-manifest\.json$/],
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
+  },
+};
+
+module.exports = withPWA(nextConfig);
